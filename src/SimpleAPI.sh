33ac37702e040644c6833d79c146fdcf090a697a
laughingbiscuit <sean@laughingbiscuit.com>
2023-06-28 20:24:01 +0000

    kind example

--- src/SimpleAPI.sh
+++ src/SimpleAPI.sh
@@ -5,8 +5,36 @@ docker rm -f httpbin
 docker run -id --name some-nginx -p=80:80 nginx
 docker network ls
 
-sleep 5
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
 
-curl -v http://localhost || true
-curl -v http://127.0.0.1 || true
-curl -v http://0.0.0.0 || true
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/usage.yaml
+
+
+
+sleep 30
+curl -v http://localhost/foo/hostname || true
