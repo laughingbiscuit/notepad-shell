07a74b1fe53634e7f053500213a5d5553159dda2
laughingbiscuit <sean@laughingbiscuit.com>
2023-06-28 19:56:51 +0000

    try with nginx

--- src/SimpleAPI.sh
+++ src/SimpleAPI.sh
@@ -2,11 +2,8 @@
 set -xe
 
 docker rm -f httpbin
-docker run --name httpbin --network "host" kennethreitz/httpbin 
-docker run --name httpbin2 -d -p 80:80 kennethreitz/httpbin 
+docker run --name some-nginx --net=host nginx
 
-sleep 15
+sleep 5
 
-curl -isS -H "Custom-Header: True" http://localhost/get
-curl -isS -H "Custom-Header: True" http://httpbin/get
-curl -isS -H "Custom-Header: True" http://httpbin2/get
+curl -H http://localhost
