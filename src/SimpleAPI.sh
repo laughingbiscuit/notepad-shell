98afa6b26344ab78629e9268ffc3325e3fbb7b4d
laughingbiscuit <sean@laughingbiscuit.com>
2023-06-28 10:51:13 +0000

    try a few hostnames

--- src/SimpleAPI.sh
+++ src/SimpleAPI.sh
@@ -2,8 +2,11 @@
 set -xe
 
 docker rm -f httpbin
-docker run --name httpbin --network "host" -d -p 80:80 kennethreitz/httpbin 
+docker run --name httpbin --network "host" kennethreitz/httpbin 
+docker run --name httpbin2 -d -p 80:80 kennethreitz/httpbin 
 
 sleep 15
 
 curl -isSf -H "Custom-Header: True" http://localhost/get
+curl -isSf -H "Custom-Header: True" http://httpbin/get
+curl -isSf -H "Custom-Header: True" http://httpbin2/get
